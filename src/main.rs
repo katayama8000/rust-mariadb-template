d1ab2c6e36e5235363850f65b4b920dd64d76567
katayama800 <tattu.0310@gmail.com>
2024-02-20 15:00:27 +0000

    done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
-fn main() {
+use sqlx::mysql::MySqlPoolOptions;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
     println!("Hello, world!");
-    let x = 5;
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect("mysql://myuser:mypassword@mariadb/mydatabase")
+        .await?;
+
+    // insert some data
+    sqlx::query("INSERT INTO mytable (name) VALUES (?)")
+        .bind("World")
+        .execute(&pool)
+        .await?;
+    // fetch all
+    let rows = sqlx::query("SELECT * FROM mytable")
+        .fetch_all(&pool)
+        .await?;
+    println!("Got {} rows", rows.len());
+    Ok(())
 }
